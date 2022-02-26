9e84c82140c6a28c396c7b1b34fdb57e03305646
sovlookup <gonorth@qq.com>
2022-02-26 23:54:37 +0800

    修复 上传数据集过大的问题

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,17 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      jsonLimit: "2048mb",
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
